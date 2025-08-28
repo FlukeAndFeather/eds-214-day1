f88f4e81b84d30ca75d2a622b24009b2b6f3e7ca
hylaea-miller <hylaeamiller@gmail.com>
2025-08-27 23:47:33 -0700

    plot

--- R/Project.R
+++ R/Project.R
@@ -11,21 +11,21 @@ source("R/function.R")
 
 # Data frame
 
-bq1 <- read_csv(here("data/QuebradaCuenca1-Bisley.csv"), na = "-9999", show_col_types = F) %>% 
+bq1 <- read_csv(here("data/QuebradaCuenca1-Bisley.csv")) %>% 
   clean_names() %>% 
   mutate(site = "BQ1")
 
-bq2 <- read_csv(here("data/QuebradaCuenca2-Bisley.csv"), na = "-9999", show_col_types = F) %>% 
+bq2 <- read_csv(here("data/QuebradaCuenca2-Bisley.csv")) %>% 
   clean_names() %>% 
   mutate(site = "BQ2")
 
 
-bq3 <- read_csv(here("data/QuebradaCuenca3-Bisley.csv"), na = "-9999", show_col_types = F) %>% 
+bq3 <- read_csv(here("data/QuebradaCuenca3-Bisley.csv")) %>% 
   clean_names() %>% 
   mutate(site = "BQ3")
 
 
-prm <- read_csv(here("data/RioMameyesPuenteRoto.csv"), na = "-9999", show_col_types = F) %>% 
+prm <- read_csv(here("data/RioMameyesPuenteRoto.csv")) %>% 
   clean_names() %>% 
   mutate(site ="PRM")
 
@@ -39,7 +39,7 @@ comb_table <- bind_rows(bq1, bq2, bq3, prm) %>%
   select(sample_date, k, no3_n, mg, ca, nh4_n, site, year)  # selecting variables of interest
 
 
-names(comb_table) # checking the variables names  
+names(comb_table) # Checking the variables names  
 
 
 # Checking class of the variables   
