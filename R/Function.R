5d0ce42d5a641caaaafa74e9ecc925256f1dfaf9
hylaea-miller <hylaeamiller@gmail.com>
2025-08-28 12:05:05 -0700

    close #2

--- R/Function.R
+++ R/Function.R
@@ -18,12 +18,3 @@
     
   #  - Refactor your code by writing a function to perform a task
   
-#  Q2: What are the task’s inputs? What data and parameters do you need to know to do the task?
-  
-  # focal_date, dates, conc, win_size_wks
-    
-#   Q3: What is the task’s output? Be specific! If it’s a vector, what type (e.g., character or numeric) is it and how long is it? If it’s a data frame, what are the column names and what are their types? Are there constraints on what the output can be (e.g., sorted, range limits, etc)?
-    
-  # 
-  
-#  Q4: Which lines of code in your spaghetti currently perform the task?
