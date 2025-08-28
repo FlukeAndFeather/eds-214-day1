806fca934437e245b19039cd18e573893bfe5cb6
hylaea-miller <hylaeamiller@gmail.com>
2025-08-27 21:11:00 -0700

    plot k

--- scratch/Spaghetti.R
+++ scratch/Spaghetti.R
@@ -4,6 +4,7 @@ library(readr)
 library(lubridate)
 library(patchwork)
 library(here)
+library(paletteer)
 
 source("R/function.R")
 
@@ -134,11 +135,19 @@ ggplot(data_frame_BQ1 = group_join) +
   
   
   #select variables of interest
-  comb_table %>% select(sample_date, sample_id, sample_time, no3_n, k, site, year, month, str_ym_date) 
+  comb_table %>% select(sample_date, sample_id, sample_time, no3_n, k, site, year, month) %>%
+    
+  
+  comb_table$k <- as.numeric(comb_table$k)
   
   # Checking class of the variables   
   class(comb_table$no3_n)
   class(comb_table$k)
+  
+
+  
+ 
+  
   
   # filter to months of interest
   
@@ -147,30 +156,74 @@ ggplot(data_frame_BQ1 = group_join) +
   
   # Create vectors to use in the function
   
-  Dates <- comb_table_subset_date$sample_date
+  dates <- comb_table_subset_date$sample_date
   
-  Concentration <- comb_table_subset_date$k
+  conc <- comb_table_subset_date$k
   
-  # Creating function of the moving average
-  
-  moving_ave <- function(focal_date, Dates, Concentration, win_size_wks) {
-    
-    is_in_window <- (Dates > focol_date - (win_size_wks / 2) * 7) & (Dates > focal_date + (win_size_wks / 2) * 7)
-    
-    window_conc <- Concentration [is_in_window]
-    
-    result <- mean(window_conc) 
-  }
-  
-  
-  
-  comb_table_subset_date$final_average <- sapply(
-    comb_table_subset_date$sample_date,
-    moving_ave,
-    dates = comb_table_subset_date$sample_date,
-    conc = comb_table_subset_date$k,
-    win_size_wks = 9
-  )
+  # calling function of the moving average
 
   
+
   
+k_ma <- comb_table_subset_date$final_average <- sapply(
+comb_table_subset_date$sample_date,
+moving_average,
+dates = comb_table_subset_date$sample_date,
+conc = comb_table_subset_date$k,
+win_size_wks = 9
+  )
+  
+  view(k_ma)
+ 
+  class(df_k$year)
+  class(comb_table$k)  
+   
+  
+df_k <- comb_table_subset_date %>% select(site, sample_date, year) %>% 
+  mutate(conc_k = k_ma) %>% 
+  group_by(site, year) %>% 
+  summarise(conc_k = mean(conc_k, na.rm = TRUE), .groups = "drop")
+
+ggplot(df_k, aes(x =year, y = conc_k, color = site, group = site)) +
+  geom_line() +
+  labs(title = "Potassium per year",
+       x = "Year",
+       y = "K")
+
+
+df_k <- df_k %>%
+  group_by(site, sample_date) %>%
+  summarise(conc_k = mean(conc_k, na.rm = TRUE), .groups = "drop")
+
+ggplot(df_k, aes(x = sample_date, y = conc_k, color = site)) +
+  geom_line() +
+  facet_wrap(~ site, scales = "free_y")
+
+ggplot(df_k, aes(x = sample_date, y = conc_k, color = site, group = site)) +
+  geom_line() +
+  labs(title = "Potassium per year",
+       x = "Year",
+       y = "K")
+
+moving_ave <- function(focal_date, dates, conc, win_size_wks) {
+  is_in_window <- (dates > focal_date - (win_size_wks/2) * 7) &
+    (dates < focal_date + (win_size_wks/2) * 7)
+  window_conc <- conc[is_in_window]
+  mean(window_conc, na.rm = TRUE)
+}
+
+df_k <- comb_table_subset_date %>%
+  group_by(site) %>%
+  mutate(conc_k = sapply(sample_date,
+                         moving_ave,
+                         dates = sample_date,
+                         conc = k,
+                         win_size_wks = 9))
+
+plot_k <- ggplot(df_k, aes(x = sample_date, y = conc_k, color = site)) +
+  geom_line() +
+  scale_y_continuous(limits = c(0.4, 1.6)) +
+  scale_color_paletteer_d("wesanderson::Darjeeling2") +
+  labs(title, x = "Year",
+       y = "K mg l⁻¹") +
+  theme_minimal()
--- scratch/Tests.R
+++ scratch/Tests.R
@@ -39,11 +39,13 @@ comb_table <- bind_rows(bq1, bq2, bq3, prm) %>% #since all of the columns are th
 
 
 #select variables of interest
-comb_table %>% select(sample_date, sample_id, sample_time, no3_n, k, site, year, month, str_ym_date) 
+comb_table %>% select(sample_date, sample_id, sample_time, no3_n, k, site, year, month, sample_date) 
 
 # Checking class of the variables   
 class(comb_table$no3_n)
 class(comb_table$k)
+class(comb_table_subset_date$sample_date)
+comb_table_subset_date$sample_date <- as.Date(comb_table_subset_date$sample_date)
 
 # filter to months of interest
 
@@ -81,43 +83,3 @@ comb_table_subset_date$final_average <- sapply(
 
 
 
-#group by date and site to summarize the mean potassium value for each site and each date
-graph_data <- comb_table_subset_date %>%
-  group_by(day, site) %>%
-  summarise(mean_k = mean(k, na.rm = TRUE),
-            mean_no_3 = mean(no3_n, na.rm = TRUE))
-
-#add in a column that identifies hurricane hugo 
-graph_data_with_hugo_highlight <- graph_data %>%
-  group_by(site) %>%
-  mutate(hugo = ifelse(str_ym_date == lubridate::ymd("1989-09-01"), "1", "0"))
-
-#creating the plot for potassium (K)
-ggplot(data = graph_data_with_hugo_highlight, aes(x = str_ym_date, y = mean_k, fill = hugo)) +
-  geom_col() +
-  facet_wrap(~site) +
-  labs(x = "Date",
-       y = "Mean Potassium (K) in mg/L",
-       title = "Mean Potassium levels from September 1988 to 1990",
-       subtitle = "Potassium spiked following Hurricane Hugo (denoted in red), fell three months later, rose back up to elevated levels, and then slowly started to return to baseline") + 
-  theme(panel.background = element_rect(fill = "white")) +
-  scale_fill_manual(values = c("1"="cyan", "0"="orange" ), guide = FALSE)
-
-#saving the plot
-ggsave(here("potassium_by_site_bar_chart.jpg"))
-
-
-
-#creating the plot for nitrate nitrogen (no_3)
-ggplot(data = graph_data_with_hugo_highlight, aes(x = str_ym_date, y = mean_no_3, fill = hugo)) + 
-  geom_col() +
-  facet_wrap(~site) +
-  labs(x = "Date",
-       y = "Mean Nitrate Nitrogen (µg/L)",
-       title = "Mean Nitrate Nitrogen levels from September 1988 to 1990",
-       subtitle = "Nitrate Nitrogen levels fell sharply during Hurricane Hugo (denoted in red) and then oscillated in the following months") + 
-  theme(panel.background = element_rect(fill = "white")) +
-  scale_fill_manual(values = c("1"="cyan", "0"="orange" ), guide = FALSE)
-
-#saving the plot
-ggsave(here("nitrate_by_site_bar_chart.jpg"), height = 10, width = 12)
